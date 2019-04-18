5f194d78372b7da15dbc3194b97097421f57a6b4
rhetoricalraj <rajpsheth@gmail.com>
2019-04-18 22:11:29 +0530

    delete_category

---
+++ delete_category.php
@@ -0,0 +1,18 @@
+<?php 
+
+	include_once('resources/init.php');
+
+	//print_r($_GET);
+if (!isset($_GET['id']) ) {
+	header('location: index.php');
+	die();
+}
+
+// fun_del_category() is defined in /resources/fun diretory
+
+if(fun_del_category($_GET['id']) == true){
+	header('location: success.php?type=delete');
+}
+
+// header('location: category_list.php');
+// die();
