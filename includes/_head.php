d756ed86e88e660d37d81ef574b9a34ac55dab67
rhetoricalraj <rajpsheth@gmail.com>
2019-04-18 21:11:08 +0530

    Added navigation bar and header file

---
+++ includes/_head.php
@@ -0,0 +1,25 @@
+<head>
+<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        
+        <!--<meta name="viewport" content="width=device-width">-->
+        <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+        
+        <!-- Styles -->
+        <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/main.css">
+        <link href="http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+        
+        <!-- Helping out oldtimers -->
+        <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+        
+        
+        <!-- Google fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+        <link href="http://fonts.googleapis.com/css?family=Lato:300|Grand+Hotel" rel="stylesheet" type="text/css" />
+        <link href="http://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed|Alegreya:700" rel="stylesheet" type="text/css" />
+        <link href="http://fonts.googleapis.com/css?family=Josefin+Sans:700|Amatic+SC:700" rel="stylesheet" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+</head>
